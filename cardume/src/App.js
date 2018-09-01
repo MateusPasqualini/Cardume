0c3592b3d8b489b46196e780cadbaa12fcbd7d0c
Mateus Victória <mateus.victoria@acad.pucrs.br>
2018-09-01 17:18:47 -0300

    conexao BD

--- cardume/src/App.js
+++ cardume/src/App.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 class App extends Component {
+
+// default State object
+estado = {
+  eventos: []
+};
+
+componentDidMount() {
+  axios
+    .get("placeholder")
+    .then(response => {
+
+      // cria um array de eventos
+      const novosEventos = response.data.map(c => {
+        return {
+            id: 1,
+		        titulo: "Evento A",
+		        atividade: "Atividade A",
+		        proprietario: "Empresa A",
+            qr: "http://link.do.qr-code"
+         };
+      });
+
+      // cria um novo objeto de "estado" sem alterar o objeto de estado original
+       
+      const novoEstado = Object.assign({}, this.estado, {
+        eventos: novosEventos
+      });
+
+      // store the new state object in the component's state
+      this.setState(novoEstado);
+    })
+    .catch(error => console.log(error));
+}
+
+
+
   render() {
     return (
       <div className="App">
