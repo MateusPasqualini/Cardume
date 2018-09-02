95fcac9a022e9586ed2a0a9603d111f7cefb503d
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2018-09-02 02:40:34 -0300

    rotas

--- cardume/src/App.js
+++ cardume/src/App.js
@@ -4,8 +4,8 @@ import Detalhes from './components/detalhes';
 import CadastroEvento from './components/cadastroEvento';
 import './css/App.css';
 import Eventos from './components/eventos'
-import axios from 'axios';
 import logo from './cardume.svg';
+import back from './assets/back.svg';
 
 
 class App extends Component {
@@ -15,16 +15,39 @@ class App extends Component {
 
     this.handleClick = this.handleClick.bind(this);
     this.handleClickEvento = this.handleClickEvento.bind(this);
+    this.handleClickBack = this.handleClickBack.bind(this);
+    this.handleClickDetalhes = this.handleClickDetalhes.bind(this);
+    this.handleClickIni = this.handleClickIni.bind(this);
   }
   handleClick(e) {
-    this.setState({ page: 'confirmP' });
+    this.setState({ page: 'detalhes' });//confirmPresenca
   }
 
   handleClickEvento(e) {
     this.setState({ page: 'cadastroEven' });
   }
+
+  handleClickIni(e) {
+    this.setState({page: ''})
+  }
+
+  handleClickBack(e) {
+    let retorno = null;
+    if(this.state.page === 'cadastroEven')
+      retorno = '#'; //ainda não tenho pra onde voltar
+    else if(this.state.page === 'confirmPresenca')
+      retorno = 'detalhes';
+    else if(this.state.page === 'detalhes')
+      retorno = '';
+    
+    this.setState({page: retorno});
+  }
+
+  handleClickDetalhes(e){
+    this.setState({ page: 'confirmPresenca' });  
+  }
   componentDidMount() {
-  let url = 'https://cardume.herokuapp.com/eventos'
+  let url = 'https://cardume.herokuapp.com/eventos/lista'
    fetch(url).
     then(response => response.json()).then((resp) => {
         console.log(resp);
@@ -34,24 +57,25 @@ class App extends Component {
       });
 
 }
-
-
-
   render() {
     const pagina = this.state.page;
     const detalhes = this.state.detalhes;
     return (
       <div className="App">
         <header className="App-header">
+        <img onClick={this.handleClickBack} src={back} alt='logo'/>
          <img src={logo} alt='logo'/>
          </header>
          {!pagina ?
           <Eventos eve={detalhes} />
-          : pagina ==='confirmP' &&  <PresencaConfirmada />}
-        {pagina==='cadastroEven' && <CadastroEvento/>}
-          {!pagina && <button  onClick={this.handleClick} type="button" class="btn btn-primary">Confirmar Presença</button>}
-          {(pagina==='confirmP') && <button  onClick={this.handleClickEvento} type="button" class="btn btn-primary">Cadastro de Evento</button>}
+          : pagina ==='confirmPresenca' &&  <PresencaConfirmada />}
+          {pagina==='cadastroEven' && <CadastroEvento/>}
+          {pagina==='detalhes' && <Detalhes det={detalhes}/>}
+          {!pagina && <button  onClick={this.handleClick} type="button" class="btn btn-primary">Quero saber mais</button>}
+          {(pagina==='detalhes') && <button  onClick={this.handleClickDetalhes} type="button" class="btn btn-primary">Quero ajudar!</button>}
+          {(pagina==='confirmPresenca') && <button  onClick={this.handleClickIni} type="button" class="btn btn-primary">Ir para a página inicial</button>}
           {(!pagina && pagina==='cadastroEven') && <button  onClick={this.handleClickEvento} type="button" class="btn btn-primary">Primary</button>}
+          {!pagina && <button  onClick={this.handleClickEvento} type="button" class="btn btn-primary">Cadastro de evento</button>}
       </div>
     );
   }
