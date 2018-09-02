c0f1b91d44a71946d7ccdd3eaaf8b047b1c4971f
rroggia <renanzr@gmail.com>
2018-09-02 13:40:09 -0300

    fix maps

--- cardume/src/components/detalhes.js
+++ cardume/src/components/detalhes.js
@@ -16,7 +16,7 @@ class Detalhes extends React.Component {
                 <p>{dateFormat(this.props.det.inicio, "dddd, mmmm dS, yyyy, h:MM:ss TT")}</p>
                 <p>{dateFormat(this.props.det.fim, "dddd, mmmm dS, yyyy, h:MM:ss TT")}</p>
                 <p>{this.props.det.adress}</p>
-                <img src="https://maps.googleapis.com/maps/api/staticmap?center=51.477222,0&zoom=14&size=400x400&key=<AIzaSyAcm-qjeL6IiBFUcnjrbdPbzNjqPuW4FcI>"/>
+                <img src="https://maps.googleapis.com/maps/api/staticmap?center=51.477222,0&zoom=14&size=400x400&key=AIzaSyAcm-qjeL6IiBFUcnjrbdPbzNjqPuW4FcI"/>
                 <p>{this.props.det.duration}</p>
             </div>
         );
