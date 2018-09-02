608b6d592abdd071bbf66c59025518be919c5506
rroggia <renanzr@gmail.com>
2018-09-02 13:30:22 -0300

    adicionando tag e img com api do google

--- cardume/src/components/detalhes.js
+++ cardume/src/components/detalhes.js
@@ -16,6 +16,7 @@ class Detalhes extends React.Component {
                 <p>{dateFormat(this.props.det.inicio, "dddd, mmmm dS, yyyy, h:MM:ss TT")}</p>
                 <p>{dateFormat(this.props.det.fim, "dddd, mmmm dS, yyyy, h:MM:ss TT")}</p>
                 <p>{this.props.det.adress}</p>
+                <img src="https://maps.googleapis.com/maps/api/staticmap?center=51.477222,0&zoom=14&size=400x400&key=<BIANCA_ADICIONAR_CHAVE_AQUI>"/>
                 <p>{this.props.det.duration}</p>
             </div>
         );
