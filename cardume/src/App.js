8fc4d972c85ab3d53c5f4242b77877089670bb2d
Mateus Victória <mateus.victoria@acad.pucrs.br>
2018-09-01 23:28:37 -0300

    cadastro

--- cardume/src/App.js
+++ cardume/src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import './App.css';
 import ConfirmarPresenca from './components/confirmarPresenca';
 import Detalhes from './components/detalhes';
 import CadastroEvento from './components/cadastroEvento';
-=======
 import './css/App.css';
 import Eventos from './components/eventos'
-import axios from 'axios';
 
-import ConfirmarPresenca from './components/confirmarPresenca'
->>>>>>> 0dce0221db9589d8fd62a6d46906eccc2c6c0176
 
 class App extends Component {
   constructor(props) {
@@ -33,39 +27,6 @@ class App extends Component {
         });
       });
 
-<<<<<<< HEAD
-      console.log("AQUI:"+this.state.detalhes.titulo);
-  //   axios
-  //   .get(`https://cardume.herokuapp.com/eventos`)
-  //   .then(response => {
-  //     this.setState({ eventos: response });  
-  //     console.log("ESSE" + response.json());
-
-  // //  //   cria um array de eventos
-  // //     const novosEventos = response.map(c => {
-  // //       return {
-  // //           id: c.description,
-	// // 	        titulo: "Evento A",
-	// // 	        atividade: "Atividade A",
-	// // 	        proprietario: "Empresa A",
-  // //           qr: "http://link.do.qr-code"
-  // //        };
-  // //     });
-
-  // //     // cria um novo objeto de "estado" sem alterar o objeto de estado original
-       
-  // //     const novoEstado = Object.assign({}, this.state.eventos, {
-  // //       eventos: novosEventos
-  // //     });
-
-  // //     // // store the new state object in the component's state
-  // //     this.setState(novoEstado);
-  // //     console.log(this.state.eventos.description)
-  //   })
-  //    .catch(error => console.log(error));
-=======
-      console.log("AQUI:"+this.state.eventos.titulo);
->>>>>>> 0dce0221db9589d8fd62a6d46906eccc2c6c0176
 }
 
 
@@ -78,17 +39,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
           {!pagina ?
-<<<<<<< HEAD
           <Detalhes det={detalhes} />
         : <ConfirmarPresenca />}
-          <CadastroEvento/>
-          <button  onClick={this.handleClick} type="button" class="btn btn-primary">Primary</button>
-              <p>{}</p>
-=======
-          <Eventos eve={eventos} />
-        : <ConfirmarPresenca />}
+        <CadastroEvento/>
           {pagina && <button  onClick={this.handleClick} type="button" class="btn btn-primary">Primary</button>}
->>>>>>> 0dce0221db9589d8fd62a6d46906eccc2c6c0176
         </header>
       </div>
     );
--- cardume/src/components/cadastroEvento.js
+++ cardume/src/components/cadastroEvento.js
@@ -3,33 +3,66 @@ import React, { Component } from 'react';
 
 
 class CadastroEvento  extends React.Component {
-        constructor(props) {
-          super(props);
-          this.state = {value: ''};
-      
-          this.handleChange = this.handleChange.bind(this);
-          this.handleSubmit = this.handleSubmit.bind(this);
-        }
-      
-        handleChange(event) {
-          this.setState({value: event.target.value});
-        }
-      
-        handleSubmit(event) {
-          alert('A name was submitted: ' + this.state.value);
-          event.preventDefault();
-        }
-      
-        render() {
-          return (
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-              </label>
-              <input type="submit" value="Submit" />
-            </form>
-          );
-        }
-    }         
+    constructor(props) {
+        super(props);
+        this.state = {
+          description: '',
+          titulo: '',
+          inicio: '',
+          fim: ''
+        };
+    
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+    
+      handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+    
+      render() {
+        return (
+          <form>
+            <label>
+              description:
+              <input
+                name="Description"
+                type="text"
+                checked={this.state.description}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+              titulo:
+              <input
+                name="Titulo"
+                type="text"
+                value={this.state.titulo}
+                onChange={this.handleInputChange} />
+            </label><br />
+            <label>
+                inicio:
+                <input
+                name="Inicio"
+                type="text"
+                value={this.state.inicio}
+                onChange={this.handleInputChange} />                
+            </label><br />
+            <label>
+                fim:
+                <input
+                name="Fim"
+                type="text"
+                value={this.state.fim}
+                onChange={this.handleInputChange} />                
+            </label><br />
+          </form>
+        );
+      }
+    }           
 export default CadastroEvento;
