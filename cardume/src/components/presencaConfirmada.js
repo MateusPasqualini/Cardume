175c0213af645bec3289144012cd46f4f118e042
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2018-09-02 05:03:52 -0300

    botões

--- cardume/src/components/presencaConfirmada.js
+++ cardume/src/components/presencaConfirmada.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class PresencaConfirmada  extends React.Component {
     render() { 
         return (
-            <div className="cardBig">
+            <div className="card cardBig">
                 <p className="card-title">Obrigado por nos ajudar, agora você é mais um peixinho!<br/> Nos vemos no próximo evento!</p>
                 
             </div>
